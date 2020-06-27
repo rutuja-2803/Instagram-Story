6bad66305c65d61f435dc4ceafa0284b22c3dd3c
rutuja-2803 <67337174+rutuja-2803@users.noreply.github.com>
2020-06-28 01:29:01 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ class App extends Component {
             
             <div className="main"><br/>
                 <div className="rounded1">
-                    <img src={process.env.PUBLIC_URL + "/img2.jpeg"} className="rounded" width = "50px" height = "50px"/>
+                    <img src={process.env.PUBLIC_URL + "/img3.jpeg"} className="rounded" width = "50px" height = "50px"/>
                 </div>
                 <div className="name1">
-                     pratik08_
+                     pratii_s05
                 </div>
                 
                 <div className="tag1">
@@ -22,19 +22,19 @@ class App extends Component {
                     <hr/>
                 </div><br/>
                 <div className="chat1">
-                    Hey Pratik!!
+                    Hey!!<br/>Hapieee Birthdayyy
                 </div><br/>
                 <div className="chat2">
-                    Hieee 
+                    Thank you soo muchh!!! 
                 </div>    <br/>
                 <div className="chat3">
-                    I need your help!    
+                    Soo!!<br/>Where's the party?
                 </div>  <br/>
                 <div className="chat4">
-                    For??
+                    At my home..<br/>Be there at sharp 5:00
                 </div> <br/>
                 <div className="chat5">
-                    Making pizza's!!    
+                    Sure..meet you soon:)    
                 </div> 
                 <div className="tag2">
                     <svg aria-label="Emoji" class="_8-yf5 " fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M24 48C10.8 48 0 37.2 0 24S10.8 0 24 0s24 10.8 24 24-10.8 24-24 24zm0-45C12.4 3 3 12.4 3 24s9.4 21 21 21 21-9.4 21-21S35.6 3 24 3z"></path><path d="M34.9 24c0-1.4-1.1-2.5-2.5-2.5s-2.5 1.1-2.5 2.5 1.1 2.5 2.5 2.5 2.5-1.1 2.5-2.5zm-21.8 0c0-1.4 1.1-2.5 2.5-2.5s2.5 1.1 2.5 2.5-1.1 2.5-2.5 2.5-2.5-1.1-2.5-2.5zM24 37.3c-5.2 0-8-3.5-8.2-3.7-.5-.6-.4-1.6.2-2.1.6-.5 1.6-.4 2.1.2.1.1 2.1 2.5 5.8 2.5 3.7 0 5.8-2.5 5.8-2.5.5-.6 1.5-.7 2.1-.2.6.5.7 1.5.2 2.1 0 .2-2.8 3.7-8 3.7z"></path></svg>
